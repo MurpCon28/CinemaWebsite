38c84587f74ddacccd382ba675b404e295e087d1
MurpCon28 <conormu4phy@gmail.com>
2022-02-22 16:10:53 +0000

    2nd year project

---
+++ Website/js/javascript.js
@@ -0,0 +1,11 @@
+//$(document).ready(function() {
+//
+//  
+//$('[data-toggle="collapse"]').click(function() {
+//  $(this).toggleClass( "active" );
+//  if ($(this).hasClass("active")) {
+//    $(this).text("EXPAND");
+//  } else {
+//    $(this).text("SHOW LESS");
+//  }
+//});
